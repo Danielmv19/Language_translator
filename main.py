49b007f0916a69aa04df8cddaadc4a3c66a6770a
Valentine <78097863+Danielmv19@users.noreply.github.com>
2023-08-28 21:04:14 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,198 @@
+
+'''⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⠤⠤⠠⡖⠲⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⡠⠶⣴⣶⣄⠀⠀⠀⢀⣴⣞⣼⣴⣖⣶⣾⡷⣶⣿⣿⣷⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⢸⠀⠀⠀⠙⢟⠛⠴⣶⣿⣿⠟⠙⣍⠑⢌⠙⢵⣝⢿⣽⡮⣎⢿⡦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⢱⡶⣋⠿⣽⣸⡀⠘⣎⢢⡰⣷⢿⣣⠹⣿⢸⣿⢿⠿⡦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⢧⡿⣇⡅⣿⣇⠗⢤⣸⣿⢳⣹⡀⠳⣷⣻⣼⢿⣯⡷⣿⣁⠒⠠⢄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⣼⣿⣧⡏⣿⣿⢾⣯⡠⣾⣸⣿⡿⣦⣙⣿⢹⡇⣿⣷⣝⠿⣅⣂⡀⠀⠡⢂⠄⣀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠇⠀⠀⠀⠀⣿⡟⣿⡇⡏⣿⣽⣿⣧⢻⡗⡇⣇⣤⣿⣿⣿⣧⣿⣿⡲⣭⣀⡭⠛⠁⠀⠀⠈⠀⠉⢂⢄⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠸⠀⠀⠀⠀⢻⣿⣇⣥⣏⣘⣿⣏⠛⠻⣷⠿⡻⡛⠷⡽⡿⣿⣿⣿⣷⠟⠓⠉⠢⢄⡀⠀⠀⠀⠀⠀⠁⠫⢢⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢇⠀⠀⠀⢸⣾⣿⣽⣿⣏⣻⠻⠁⢠⠁⠀⠀⠀⠘⣰⣿⣿⢟⢹⢻⠀⠀⠀⠀⠀⠈⠒⢄⡀⠀⠀⠀⠀⠀⠀⠑⢄
+⠀⠀⠀⠀⠀⠀⠘⡄⠀⠀⢸⣯⣿⣿⣿⢷⡀⠀⠀⠀⠀⠀⠀⠀⠛⣩⣿⣿⢿⣾⣸⠀⠀⠀⠀⠀⠀⢀⡠⠚⠉⠉⠁⠀⠀⠀⢀⠌
+⠀⠀⠀⠀⠀⠀⠀⢡⠀⠀⠀⢟⣿⣯⡟⠿⡟⢇⡀⠀⠀⠐⠁⢀⢴⠋⡼⢣⣿⣻⡏⠀⠀⠀⣀⠄⠂⠁⠀⠀⠀⠀⠀⠀⢀⡤⠂⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠇⠀⠀⠈⠊⢻⣿⣜⡹⡀⠈⠱⠂⠤⠔⠡⢶⣽⡷⢟⡿⠕⠒⠀⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⡠⠐⠁⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠘⡄⠀⠀⠀⠀⢿⠿⠿⢿⠾⣽⡀⠀⠀⠀⠈⠻⣥⣃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠤⠒⠁⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⡀⡀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣖⠂⠠⠐⠋⠀⠙⠳⣤⣠⠀⠀⠀⣀⠤⠒⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠵⡐⠄⠀⠀⠀⠀⠀⠀⠀⠈⢷⣄⡀⠀⠠⡀⠀⠈⠙⠶⣖⡉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⡥⠈⠂⠀⠀⠀⠀⠀⠀⠀⣼⠉⠙⠲⣄⠈⠣⡀⠀⠀⠈⢻⡦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⡄⠀⠀⠀⠀⠀⠀⠀⢠⠇⠀⠀⠀⠈⣷⡄⠈⠄⠀⠀⠀⢧⠀⠑⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⡄⠀⠀⠀⡀⠀⢠⣿⣤⣤⣶⣶⣾⣿⣿⡄⢸⠀⠀⠀⢸⣄⣤⣼⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⡄⠀⠀⠇⣠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⢸⠀⠀⠀⣼⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣀⣀⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡆⠀⢀⣼⣿⣿⣿⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠉⠁⠀⠈⠉⠙⠛⠿⠿⠽⠿⠟⠛⡉⠛⠲⣿⣿⠿⡿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡇⠀⠀⢠⡏⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠋⠀⠀⣠⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡰⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢔⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡠⠊⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⠒⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠄⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢀⡠⠊⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠊⠀⠀⠀⠀⠀⣃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⡠⣻⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⢫⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣰⡿⣿⣿⣦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣼⠏⣸⣿⣷⢷⠙⣻⢶⣤⣄⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⠾⠉⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠰⣏⠀⣿⣿⡘⣼⡇⠀⠁⠙⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠉⠁⠀⠀⣽⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⢙⠓⠛⠘⣧⠾⢷⣄⠀⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⠿⠋⠀⠀⠀⠀⠀⠀⣿⢟⢇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠸⠀⠀⠀⢸⣧⠀⠹⣆⠀⠀⠀⠀⠈⢻⣿⣿⡿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⣿⢂⠙⢿⡷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢃⠀⠀⠈⠙⠀⠀⠻⡄⠀⠀⠀⠀⠸⡀⠹⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡾⠐⠠⠀⠻⠬⠄⡒⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠈⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢣⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠘⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠐⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠑⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⡀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠃⠀⠀⠀⠀
+'''
+import customtkinter
+import tkinter
+from deep_translator import MyMemoryTranslator
+import pprint as pp
+import pyclip
+from datetime import date
+from tkinter import filedialog as fd
+from CTkToolTip import *
+from gtts import gTTS
+from playsound import playsound
+from CTkMessagebox import *
+
+# TEMA
+customtkinter.set_appearance_mode("system")
+customtkinter.set_default_color_theme("blue")
+# VENTANA
+app = customtkinter.CTk()
+app.geometry('800x500')
+app.title('Traductor pro max super :v')
+
+idiomas = ['en-US', 'es-US', 'ja-JP', 'fr-FR', 'ru-RU']
+
+# ingles = 'en-US'
+# español = 'es-419'
+# japones = 'ja-JP'
+# frances = 'fr-FR'
+# ruso = 'ru-RU'
+
+# COMBOBOX FUN N1
+def combobox_callback1(choice):
+    print(choice)
+# COMBOBOX N1
+combobox1 = customtkinter.CTkComboBox(app, values=idiomas,command=combobox_callback1)
+combobox1.place(relx=0.25, rely=0.32, anchor=tkinter.CENTER)
+combobox1.set("en-US")
+# COMBOBOX FUN N2
+def combobox_callback2(choice):
+    print(choice)
+# COMBOBOX N2
+combobox2 = customtkinter.CTkComboBox(app, values=idiomas, command=combobox_callback2)
+combobox2.place(relx=0.75, rely=0.32, anchor=tkinter.CENTER)
+combobox2.set("es-US")
+translated = ''
+def get_text_textbox1():
+    global translated
+    text = textbox1.get("0.0", "end")
+    translated = MyMemoryTranslator(source=combobox1.get(), target=combobox2.get()).translate(text=text) # en-US
+    pp.pprint(f'texto ingresado >> {text}')
+    pp.pprint(f'texto traducido >> {translated}')
+    textbox2.insert("0.0", translated)
+    #textbox2.configure(state='disabled')
+# TEXTBOX N1
+textbox1 = customtkinter.CTkTextbox(app, border_color='black', border_width=1)
+textbox1.place(relx=0.25, rely=0.6, anchor=tkinter.CENTER)
+textbox1.configure(width=300, height=200)
+textbox1.focus_set()
+# TEXTBOX N2
+textbox2 = customtkinter.CTkTextbox(app)
+textbox2.place(relx=0.75, rely=0.6, anchor=tkinter.CENTER)
+textbox2.configure(width=300, height=200, border_color='black', border_width=1)
+# BOTON N1
+button1 = customtkinter.CTkButton(app, text="Traducir")
+button1.place(relx=0.5, rely=0.9, anchor=tkinter.CENTER)
+button1.configure(width=175, height=35,  command=lambda: get_text_textbox1())
+CTkToolTip(button1, message="Presiona para traducir")
+
+
+# BOTON N2
+button2 = customtkinter.CTkButton(app, text="<->", state='disabled')
+button2.place(relx=0.5, rely=0.32, anchor=tkinter.CENTER)
+button2.configure(width=10, height=10)
+CTkToolTip(button2, message="Cambiar")
+def copy_event():
+    pyclip.copy(translated)
+    print('texto copiado')
+
+# BOTON N3
+button3 = customtkinter.CTkButton(app, text="Copiar", command=lambda: copy_event())
+button3.place(relx=0.9, rely=0.83, anchor=tkinter.CENTER)
+button3.configure(width=10, height=10)
+CTkToolTip(button3, message="Copiar texto al portapapeles")
+
+def delete_event():
+    try:
+        textbox2.delete(index1='0.0', index2='end')
+    except:
+        print('hubo un error al borrar por favor suicidate')
+# BOTON N4
+button4 = customtkinter.CTkButton(app, text="Borrar", command=lambda: delete_event())
+button4.place(relx=0.83, rely=0.83, anchor=tkinter.CENTER)
+button4.configure(width=10, height=10)
+CTkToolTip(button4, message="Borrar traduccion")
+def save_event():
+    try:
+        archivo = open(f"{date.today()}.txt", 'w')
+        archivo.write(translated)
+        archivo.close()
+    except:
+        print('hubo un error al guardar por favor suicidate')
+
+# BOTON N5
+button5 = customtkinter.CTkButton(app, text="Guardar", command=lambda: save_event())
+button5.place(relx=0.755, rely=0.83, anchor=tkinter.CENTER)
+button5.configure(width=10, height=10)
+CTkToolTip(button5, message="Guardar en un archivo txt")
+
+def open_event():
+    file = fd.askopenfilename()
+    archivo = open(f"{file}", 'r')
+    contenido = archivo.read()
+    textbox1.insert("0.0", contenido)
+
+# BOTON N6
+button6 = customtkinter.CTkButton(app, text="Abrir", command=lambda: open_event())
+button6.place(relx=0.09, rely=0.83, anchor=tkinter.CENTER)
+button6.configure(width=10, height=10)
+CTkToolTip(button6, message="abrir txt")
+
+def tts_event():
+    try:
+        nombre = 'tts.mp3'
+        tts = gTTS(f'{translated}', lang=combobox2.get())
+        with open(nombre, 'wb') as archivo:
+            tts.write_to_fp(archivo)
+
+        playsound(nombre)
+    except:
+        CTkMessagebox(title="Error", message="Idioma no disponible!!!", icon="cancel")
+
+button7 = customtkinter.CTkButton(app, text="Reproducir", command=lambda: tts_event())
+button7.place(relx=0.66, rely=0.83, anchor=tkinter.CENTER)
+button7.configure(width=10, height=10)
+CTkToolTip(button7, message="Reproduce la traduccion")
+# CHECKBOX N1
+def checkbox_event():
+    print("checkbox toggled, current value:", check_var.get())
+    if check_var.get() == "off":
+        customtkinter.set_appearance_mode("light")
+    else:
+        customtkinter.set_appearance_mode("dark")
+
+
+check_var = customtkinter.StringVar(value="on")
+checkbox = customtkinter.CTkCheckBox(app, text="Modo Latam", command=checkbox_event,
+                                     variable=check_var, onvalue="on", offvalue="off")
+checkbox.place(relx=0.1, rely=0.95, anchor=tkinter.CENTER)
+CTkToolTip(checkbox, message="Activar/Desactivar modo oscuro")
+
+
+
+#
+img = tkinter.PhotoImage(file="Google-translate.png")
+img = img.subsample(25)
+label1 = customtkinter.CTkLabel(app, text='', image=img, fg_color="transparent")
+label1.place(relx=0.5, rely=0.15, anchor=tkinter.CENTER)
+
+
+app.mainloop()
