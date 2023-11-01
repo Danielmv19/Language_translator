502102ebf0e0eda8c39cdc5ea41d13c7fe4b1813
Valentine <78097863+Danielmv19@users.noreply.github.com>
2023-11-01 17:05:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ import customtkinter
 import tkinter
 from deep_translator import MyMemoryTranslator
 import pprint as pp
-import pyclip
+import pyclip # linux sudo apt-get install xclip sudo apt-get install xsel
 from datetime import date
 from tkinter import filedialog as fd
 from CTkToolTip import *
@@ -59,7 +59,6 @@ app.geometry('800x500')
 app.title('Traductor pro max super :v')
 
 idiomas = ['en-US', 'es-US', 'ja-JP', 'fr-FR', 'ru-RU']
-
 # ingles = 'en-US'
 # español = 'es-419'
 # japones = 'ja-JP'
@@ -101,7 +100,7 @@ textbox2.configure(width=300, height=200, border_color='black', border_width=1)
 # BOTON N1
 button1 = customtkinter.CTkButton(app, text="Traducir")
 button1.place(relx=0.5, rely=0.9, anchor=tkinter.CENTER)
-button1.configure(width=175, height=35,  command=lambda: get_text_textbox1())
+button1.configure(width=175, height=35,  command=get_text_textbox1)
 CTkToolTip(button1, message="Presiona para traducir")
 
 
@@ -115,7 +114,7 @@ def copy_event():
     print('texto copiado')
 
 # BOTON N3
-button3 = customtkinter.CTkButton(app, text="Copiar", command=lambda: copy_event())
+button3 = customtkinter.CTkButton(app, text="Copiar", command=copy_event)
 button3.place(relx=0.9, rely=0.83, anchor=tkinter.CENTER)
 button3.configure(width=10, height=10)
 CTkToolTip(button3, message="Copiar texto al portapapeles")
@@ -126,7 +125,7 @@ def delete_event():
     except:
         print('hubo un error al borrar por favor suicidate')
 # BOTON N4
-button4 = customtkinter.CTkButton(app, text="Borrar", command=lambda: delete_event())
+button4 = customtkinter.CTkButton(app, text="Borrar", command=delete_event)
 button4.place(relx=0.83, rely=0.83, anchor=tkinter.CENTER)
 button4.configure(width=10, height=10)
 CTkToolTip(button4, message="Borrar traduccion")
@@ -139,7 +138,7 @@ def save_event():
         print('hubo un error al guardar por favor suicidate')
 
 # BOTON N5
-button5 = customtkinter.CTkButton(app, text="Guardar", command=lambda: save_event())
+button5 = customtkinter.CTkButton(app, text="Guardar", command=save_event)
 button5.place(relx=0.755, rely=0.83, anchor=tkinter.CENTER)
 button5.configure(width=10, height=10)
 CTkToolTip(button5, message="Guardar en un archivo txt")
@@ -151,7 +150,7 @@ def open_event():
     textbox1.insert("0.0", contenido)
 
 # BOTON N6
-button6 = customtkinter.CTkButton(app, text="Abrir", command=lambda: open_event())
+button6 = customtkinter.CTkButton(app, text="Abrir", command=open_event)
 button6.place(relx=0.09, rely=0.83, anchor=tkinter.CENTER)
 button6.configure(width=10, height=10)
 CTkToolTip(button6, message="abrir txt")
@@ -167,7 +166,7 @@ def tts_event():
     except:
         CTkMessagebox(title="Error", message="Idioma no disponible!!!", icon="cancel")
 
-button7 = customtkinter.CTkButton(app, text="Reproducir", command=lambda: tts_event())
+button7 = customtkinter.CTkButton(app, text="Reproducir", command=tts_event)
 button7.place(relx=0.66, rely=0.83, anchor=tkinter.CENTER)
 button7.configure(width=10, height=10)
 CTkToolTip(button7, message="Reproduce la traduccion")
